e2e3a64430bbc6b7e6170e7c7b747d6cdb836bb9
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-01-12 09:47:39 -0500

    initial commit

---
+++ EvilScientistCorporation/app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException
+
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+from app.routers import users, items
+
+# Set up FastAPI. We'll use this "app" variable to do FastAPI stuff below
+app = FastAPI()
+
+# Setting CORS (Cross Origin Resource Sharing) policy
+# origins = ["*"] # Allow all origins (not recommended for production)
+origins = ["http://localhost"] # Allows requests only from localhost
+
+#TODO for Ben: CORS middleware setup
+
+# Global custom Exception Handler
+# All Exceptions raised in the routers get handled here
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request:Request, exception:HTTPException):
+    return JSONResponse(
+        status_code=exception.status_code,
+        content={"message":exception.detail}
+    )
+
+# Import routers here
+app.include_router(users.router)
+app.include_router(items.router)
+
+# Generic sample endpoint (GET request that just returns a message)
+@app.get("/")
+async def sample_endpoint():
+    return {"message":"Hello FastAPI!"}
