36302f7d4ace57d7ca00dc911617864a493c2e1b
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-01-22 11:19:26 -0500

    NER and RAG in one endpoint! just 3 installments of 19.99

--- EvilScientistCorporation/app/routers/vector_ops.py
+++ EvilScientistCorporation/app/routers/vector_ops.py
@@ -79,5 +79,16 @@ async def ner_search_plans(request:SearchRequest):
     # Collect the extracted entities from the model
     entities = extract_entities(combined_text)
 
-    # FOR NOW: just return the entities
-    return entities
+    # # FOR NOW: just return the entities
+    # return entities
+
+    # Create a new prompt for our LLM and tell it help us with classification
+    # Another example of RAG - we're retrieving info that will augment the response
+    prompt = (
+        f"Based on the following extracted entities from a boss's evil plans,"
+        f"{entities}\n"
+        f"Answer the User's NER-based question with ONLY the data you see here"
+        f"User query: {request.query}"
+    )
+
+    return chain.invoke({"input":prompt})
