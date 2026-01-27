d30a5d35a0716993f547415cc173f95eab144c46
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-01-27 10:08:40 -0500

    halfway thru tools

--- EvilScientistCorporation/app/services/agentic_langgraph_service.py
+++ EvilScientistCorporation/app/services/agentic_langgraph_service.py
@@ -8,8 +8,11 @@ The chat nodes pretty much stay the same
 from typing import TypedDict, Any, Annotated
 
 from langchain_core.messages import BaseMessage
+from langchain_core.tools import tool
 from langchain_ollama import ChatOllama
 from langgraph.graph import add_messages
+
+from app.services.vectordb_service import search
 
 llm = ChatOllama(
     model="llama3.2:3b", # The model we're using (we installed llama3.2:3b)
@@ -24,3 +27,18 @@ class GraphState(TypedDict, total=False): #total=False makes all fields optional
     answer: str
     # with add_messages reducer
     message_memory: Annotated[list[BaseMessage], add_messages]
+
+
+# TOOLS ---------------------------
+# The agentic route node will use one or none of these based on the User's query
+# Remember, tools are just python functions that an agent CAN execute at run time
+# We also won't directly use state in a tool. The agent calls these.
+# IMPORTANT: Each tool needs '''docstrings''' to describe what they do for the agent
+
+@tool
+def extract_items_tool(query:str) -> list[dict[str, Any]]:
+    """Retrieve relevant evil items or products based on the evil_items vectorDB collection"""
+    return search(query, k=5, collection="evil_items")
+
+
+
