affb03ae313ef80e0b7ed0497e5339670d05d2fd
Joseph Vigil <joeyvigil109329@gmail.com>
2026-01-28 16:53:09 -0700

    update

--- app/routers/sql_ops.py
+++ app/routers/sql_ops.py
@@ -1,9 +1,9 @@
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.services.chain_service import get_general_chain
 
 from app.models.user_db_model import CreateUserModel, UserDBModel
+from app.services.chain_service import get_general_chain
 from app.services.db_connection import get_db
 
 # This is a router just like any other, but it interacts with a SQlite DB
@@ -33,21 +33,29 @@ async def create_user(incoming_user: CreateUserModel, db: Session = Depends(get_
     # Finally, we can return the new user!
     return user
 
+# Get All Users - basic one liner DB query
 @router.get("/")
 async def get_all_users(db: Session = Depends(get_db)):
-    users = db.query(UserDBModel).all()
-    return users
+    # Get all records in the users table (referenced by UserDBModel)
+    return db.query(UserDBModel).all()
 
-# RAG - get all users, ask llm a question about them
-# (ill just hardcode a  "What are the names of all users?" question for demo purposes   )
+# RAG - get all users, ask LLM a question about them
+# (I'll just hardcode a "tell me about the usernames" prompt)
 @router.get("/rag/usernames")
-async def rag_with_usernames(db: Session = Depends(get_db)):
-    users = db.query(UserDBModel).all()
-    usernames = [user.username for user in users]
-    chain = get_general_chain()
-    response =chain.invoke({
-        "input": f"The users in the database are: {', '.join(usernames)}. \n\n"
-                 f"Make a funny statement about these users."
-    })
-    return {"response": response, "usernames": usernames}
+async def usernames_chat(db: Session = Depends(get_db)):
 
+    users = db.query(UserDBModel).all() # rewrote get all users... lol
+
+    # get all the username as a list of strings
+    usernames = [user.username for user in users]
+
+    # use the general chain from back in week 2
+    chain = get_general_chain()
+
+    # invoke the chain with a prompt - this is RAG (Retrieval Augmented Generation).
+    response = chain.invoke({
+        "input": f"""Here's a list of innocent, harmless usernames {usernames}.
+        Tell me a funny story involving at least 2 of the usernames."""
+    })
+
+    return {"response": response, "usernames":usernames}
